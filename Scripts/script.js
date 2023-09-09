9f9c1ba8475e43aa20cb5317eeb5892fddf0ecd5
ChrisTheSpud <cristian.bendea19@gmail.com>
2023-09-09 20:44:00 +0300

    Button CSS
    Added button return to top on desktop

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,15 +1,16 @@
-//Toggle for dropdown
-function dropdownActivation() {
-    document.getElementById("navDropdown").classList.toggle("show");
-    document.getElementById("navDropdown").classList.toggle("active");
-  }
-  
-//Toggles the dropmenu off if the user clicks outside it
-  window.onclick = function(e) {
-    if (!e.target.matches('.dropbutton')) {
-    var navDropdown = document.getElementById("navDropdown");
-      if (navDropdown.classList.contains('show')) {
-        navDropdown.classList.remove('show');
-      }
+// Toggle for dropdown
+const dropdownActivation = () => {
+  const navDropdown = document.getElementById("navDropdown");
+  navDropdown.classList.toggle("show");
+  navDropdown.classList.toggle("active");
+};
+
+// Toggles the dropmenu off if the user clicks outside it
+window.onclick = (e) => {
+  if (!e.target.matches('.dropbutton')) {
+    const navDropdown = document.getElementById("navDropdown");
+    if (navDropdown.classList.contains('show')) {
+      navDropdown.classList.remove('show');
     }
   }
+};
