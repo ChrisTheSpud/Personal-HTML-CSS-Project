5c5e52e0a80f6e515972144e68ab0909bccddc20
ChrisTheSpud <cristian.bendea19@gmail.com>
2023-09-09 23:26:45 +0300

    Added text

--- Scripts/backtotop.js
+++ Scripts/backtotop.js
@@ -1,10 +1,9 @@
-
 let button = document.getElementById("topButton");
 
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
 button.style.display = "block";
 } else {
 button.style.display = "none";
@@ -14,5 +13,5 @@ button.style.display = "none";
 // On click, return to top
 function topFunction() {
 document.body.scrollTop = 0; // For Safari
-document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+document.documentElement.scrollTop = 0; // For Chrome, Firefox, Edge and Opera
 }
